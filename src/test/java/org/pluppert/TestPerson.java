1787cb437ccb629d7983ed1c5bc4e82262ca5079
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-12-07 08:40:52 +0100

    - Write unit tests for Person class field getters & setters:   - firstName   - lastName

--- src/test/java/org/pluppert/TestPerson.java
+++ src/test/java/org/pluppert/TestPerson.java
@@ -2,17 +2,71 @@ package org.pluppert;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestPerson {
-    Person person = new Person("Bosse", "Startplugg", "bosse@gmailus.com");
+    Person person;
 
+    @BeforeEach
+    public void initializePerson() {
+        this.person = new Person("Bosse", "Startplugg", "bosse@gmailus.com");
+    }
     @Test
     void canGetFirstName() {
         String expectedFirstName = "Bosse";
         String firstNameReturned = person.getFirstName();
 
         assertEquals(expectedFirstName, firstNameReturned);
+    }
+
+    @Test
+    void canSetFirstName() {
+        String expectedFirstName = "Mjau";
+        person.setFirstName(expectedFirstName);
+        String firstNameReturned = person.getFirstName();
+
+        assertEquals(expectedFirstName, firstNameReturned);
+    }
+    @Test
+    void testSetFirstNameExpectedException() {
+
+        IllegalArgumentException thrown = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+            //Code under test
+            person.setFirstName(null);
+        }, "IllegalArgumentException was expected");
+
+        Assertions.assertEquals("Not allowed to set first name to 'null'", thrown.getMessage());
+    }
+    @Test
+    void canGetLastName() {
+        String expectedLastName = "Startplugg";
+        String lastNameReturned = person.getLastName();
+
+        assertEquals(expectedLastName, lastNameReturned);
+    }
+    @Test
+    void canSetLastName() {
+        String expectedLastName = "Mjau";
+        person.setLastName(expectedLastName);
+        String lastNameReturned = person.getLastName();
+
+        assertEquals(expectedLastName, lastNameReturned);
+    }
+    @Test
+    void testSetLastNameExpectedException() {
+
+        IllegalArgumentException thrown = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    //Code under test
+                    person.setLastName(null);
+                }, "IllegalArgumentException was expected");
+
+        Assertions.assertEquals("Not allowed to set last name to 'null'", thrown.getMessage());
     }
 
 }
