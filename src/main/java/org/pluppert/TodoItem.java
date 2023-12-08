2df7c104fd2e9a76f54b1a007d2a4538793b8ae5
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-12-08 13:41:37 +0100

    - TodoItem.java   - Create class fields   - Write constructors - WIP   - Write getters & setters - WIP

--- src/main/java/org/pluppert/TodoItem.java
+++ src/main/java/org/pluppert/TodoItem.java
@@ -1,4 +1,74 @@
 package org.pluppert;
 
+import java.time.LocalDate;
+
 public class TodoItem {
+    private static int id;
+    private String title;
+    private String taskDescription;
+    private LocalDate deadLine;
+    private boolean done;
+    private Person creator;
+
+    public TodoItem(String title, Person creator) {
+        setTitle(title);
+        setCreator(creator);
+    }
+    public TodoItem(String title, Person creator, String taskDescription) {
+        this(title, creator);
+        setTaskDescription(taskDescription);
+    }
+    public TodoItem(String title, Person creator, LocalDate deadLine) {
+        this(title, creator);
+        setDeadLine(deadLine);
+    }
+    public TodoItem(String title, String taskDescription, LocalDate deadLine, Person creator) {
+        this(title, creator);
+        setTaskDescription(taskDescription);
+        setDeadLine(deadLine);
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Person getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Person creator) {
+        this.creator = creator;
+    }
 }
