1785d6b0a5d858884929db409cdc067797dcc7cd
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2024-01-08 08:15:46 +0100

    - Write interface TodoItemDAO - Implement interface methods in TodoItemDAOCollection

--- src/main/java/org/pluppert/data/TodoItemDAO.java
+++ src/main/java/org/pluppert/data/TodoItemDAO.java
@@ -1,4 +1,15 @@
 package org.pluppert.data;
 
-public interface TodoItemDAO {
+import org.pluppert.model.TodoItem;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+public interface TodoItemDAO extends BaseDAO<TodoItem, Integer> {
+    TodoItem findById(int id);
+    Collection<TodoItem> findAllByDoneStatus(boolean done);
+    Collection<TodoItem> findByTitleContains(String title);
+    Collection<TodoItem> findByPersonId(int id);
+    Collection<TodoItem> findByDeadlineBefore(LocalDate date);
+    Collection<TodoItem> findByDeadlineAfter(LocalDate date);
 }
--- src/main/java/org/pluppert/data/TodoItemDAOCollection.java
+++ src/main/java/org/pluppert/data/TodoItemDAOCollection.java
@@ -1,4 +1,89 @@
 package org.pluppert.data;
 
-public class TodoItemDAOCollection {
+import org.pluppert.model.Person;
+import org.pluppert.model.TodoItem;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TodoItemDAOCollection implements TodoItemDAO {
+    private static final TodoItemDAO INSTANCE;
+
+    static {
+        INSTANCE = new TodoItemDAOCollection();
+    }
+
+    private final List<TodoItem> todoItemList;
+
+    private TodoItemDAOCollection() {
+        todoItemList = new ArrayList<>();
+    }
+
+    static TodoItemDAO getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public TodoItem persist(TodoItem todoItem) {
+        if (todoItem == null) throw new NullPointerException("todoItem is null");
+        todoItemList.add(todoItem);
+        return todoItem;
+    }
+
+    @Override
+    public Collection<TodoItem> findAll() {
+        return List.copyOf(todoItemList);
+    }
+
+    @Override
+    public void remove(Integer id) {
+        todoItemList.remove(findById(id));
+    }
+
+    @Override
+    public TodoItem findById(int id) {
+        return findAll().stream()
+                .filter(todoItem -> todoItem.getId() == id)
+                .findAny().orElse(null);
+    }
+
+    @Override
+    public Collection<TodoItem> findAllByDoneStatus(boolean done) {
+        return findAll().stream()
+                .filter(todoItem -> todoItem.isDone() == done)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    @Override
+    public Collection<TodoItem> findByTitleContains(String title) {
+        return findAll().stream()
+                .filter(todoItem -> todoItem.getTitle().toLowerCase().contains(title.toLowerCase()))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    @Override
+    public Collection<TodoItem> findByPersonId(int id) {
+        return findAll().stream()
+                .filter(todoItem -> todoItem.getCreator().getId() == id)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    @Override
+    public Collection<TodoItem> findByDeadlineBefore(LocalDate date) {
+        return findAll().stream()
+                .filter(todoItem -> todoItem.getDeadline().isBefore(date))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    @Override
+    public Collection<TodoItem> findByDeadlineAfter(LocalDate date) {
+        return findAll().stream()
+                .filter(todoItem -> todoItem.getDeadline().isAfter(date))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
 }
+
+
